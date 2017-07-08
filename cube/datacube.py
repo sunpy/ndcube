a4dc81bf34d0171375929c694565719ee545aecb
abit2 <ankit.baruah1@gmail.com>
2017-07-08 11:01:16 +0530

    adding shape in cubesequence

--- cube/datacube.py
+++ cube/datacube.py
@@ -555,11 +555,16 @@ class CubeSequence(object):
     """
 
     def __init__(self, data_list, meta=None, common_axis=None):
-        if not all(isinstance(data, Cube) for data in data_list):
-            raise ValueError("data list should be of cube object")
+        if not isinstance(data_list, Cube):
+            if not all(isinstance(data, Cube) for data in data_list):
+                raise ValueError("data list should be of cube object")
         self.data = data_list
         self.meta = meta
         self.common_axis = common_axis
+        if isinstance(data_list, list):
+            self.shape = (len(data_list), data_list[0].shape[0], data_list[0].shape[1], data_list[0].shape[2])
+        else:
+            self.shape = tuple(data_list.shape)
 
     def __getitem__(self, item):
         if item is None or (isinstance(item, tuple) and None in item):
