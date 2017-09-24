9272d386c29f1a202032b49104ebf1543efdd2c0
DanRyanIrish <ryand5@tcd.ie>
2017-09-24 16:11:55 -0400

    Added some functions to check whether NDCubes and NDCubeSequence are equal.

--- ndcube/cube_utils.py
+++ ndcube/cube_utils.py
@@ -259,8 +259,32 @@ def _convert_cube_like_slice_to_sequence_slices(cube_like_slice, cumul_cube_leng
 
 def assert_extra_coords_equal(test_input, extra_coords):
     assert test_input._extra_coords.keys() == extra_coords.keys()
-    for key in test_input._extra_coords.keys():
+    for key in list(test_input._extra_coords.keys()):
         assert test_input._extra_coords[key]['axis'] == extra_coords[key]['axis']
-        assert np.all(test_input._extra_coords[key][
-                      'value'].value == extra_coords[key]['value'].value)
-        assert test_input._extra_coords[key]['value'].unit == extra_coords[key]['value'].unit
+        assert (test_input._extra_coords[key]['value'] == extra_coords[key]['value']).all()
+
+
+def assert_metas_equal(test_input, expected_output):
+    assert test_input.keys() == expected_output.keys()
+    for key in list(test_input.keys()):
+        assert test_input[key] == expected_output[key]
+
+
+def assert_cubes_equal(test_input, expected_cube):
+    assert type(test_input) == type(expected_cube)
+    assert np.all(test_input.mask == expected_cube.mask)
+    wcs_util.assert_wcs_are_equal(test_input.wcs, expected_cube.wcs)
+    assert test_input.missing_axis == expected_cube.missing_axis
+    assert test_input.uncertainty.array.shape == expected_cube.uncertainty.array.shape
+    assert test_input.dimensions[1] == expected_cube.dimensions[1]
+    assert np.all(test_input.dimensions[0].value == expected_cube.dimensions[0].value)
+    assert test_input.dimensions[0].unit == expected_cube.dimensions[0].unit
+    assert_extra_coords_equal(test_input._extra_coords, expected_cube._extra_coords)
+
+
+def assert_cubesequences_equal(test_input, expected_sequence):
+    assert type(test_input) == type(expected_sequence)
+    assert_metas_equal(test_input.meta, expected_sequence.meta)
+    assert test_input._common_axis == expected_sequence._common_axis
+    for i, cube in test_input.data:
+        assert_cubes_equal(cube, expected_sequence.data[i])
