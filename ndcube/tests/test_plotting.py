075bc8793d1039c8db320a67269d0ec4c171235f
DanRyanIrish <ryand5@tcd.ie>
2018-03-27 16:36:14 -0400

    Create module for testing NDCubePlotMixin.

---
+++ ndcube/tests/test_plotting.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import pytest
+import datetime
+
+import numpy as np
+import astropy.units as u
+import matplotlib
+
+from ndcube import NDCube
+
+
+def test_cube_plot_1D():
+    pass
+
+
+def test_cube_plot_2D():
+    pass
+
+
+def test_cube_plot_ND_as_2DAnimation():
+    pass
