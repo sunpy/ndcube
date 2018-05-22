b4aa3e883495c30cc9588ddf789f26dc04bd8c93
Baptiste PELLORCE <baptiste.pellorce@etu.univ-lyon1.fr>
2018-05-22 15:06:17 +0200

    Solve a plotting bug when NDCubeSequence is used as a Cube-like

--- ndcube/mixins/sequence_plotting.py
+++ ndcube/mixins/sequence_plotting.py
@@ -809,7 +809,7 @@ class ImageAnimatorCubeLikeNDCubeSequence(ImageAnimatorWCS):
             [c.dimensions[0].value for c in seq.data], dtype=int))
         # Add dimensions of length 1 of concatenated data array
         # shape for an missing axes.
-        if seq[0].wcs.naxis != len(seq.dimensions) - 1:
+        if seq[0].wcs.naxis != len(seq._dimensions) - 1:
             new_shape = list(data_concat.shape)
             for i in np.arange(seq[0].wcs.naxis)[seq[0].missing_axis[::-1]]:
                 new_shape.insert(i, 1)
