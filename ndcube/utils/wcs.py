37375c7f6297178c54196d193dbdb647f79a24f5
DanRyanIrish <ryand5@tcd.ie>
2019-05-07 15:59:02 -0400

    Add x, y, z, pixel support to IVOA mapping.

--- ndcube/utils/wcs.py
+++ ndcube/utils/wcs.py
@@ -39,7 +39,10 @@ wcs_to_ivoa = {
     "DEC-": "pos.eq.dec",
     "FREQ": "em.freq",
     "STOKES": "phys.polarization.stokes",
-    "PIXEL": "instr.pixel"
+    "PIXEL": "instr.pixel",
+    "XPIXEL": "custom:instr.pixel.x",
+    "YPIXEL": "custom:instr.pixel.y",
+    "ZPIXEL": "custom:instr.pixel.z"
     }
 wcs_ivoa_mapping = TwoWayDict()
 for key in wcs_to_ivoa.keys():
