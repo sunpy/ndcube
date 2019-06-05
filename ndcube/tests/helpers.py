09b90d130565dc1502e7cc587b97e698145520ca
Yash <yashrsharma44@gmail.com>
2019-06-06 02:31:34 +0530

    Added a helper method for comparing rec arrays

--- ndcube/tests/helpers.py
+++ ndcube/tests/helpers.py
@@ -8,6 +8,7 @@ import unittest
 import numpy as np
 
 from ndcube import utils
+from astropy.io import fits
 
 __all__ = ['assert_extra_coords_equal',
            'assert_metas_equal',
@@ -61,3 +62,52 @@ def assert_wcs_are_equal(wcs1, wcs2):
     assert list(wcs1.wcs.cdelt) == list(wcs2.wcs.cdelt)
     assert list(wcs1.wcs.cunit) == list(wcs2.wcs.cunit)
     assert wcs1.wcs.naxis == wcs2.wcs.naxis
+
+def comparerecords(a, b):
+    """
+    Compare two record arrays
+    Does this field by field, using approximation testing for float columns
+    (Complex not yet handled.)
+    Column names not compared, but column types and sizes are.
+    
+    Note: This helper method has been taken from `astropy.io.fits.tests.test_table`
+    """
+
+    nfieldsa = len(a.dtype.names)
+    nfieldsb = len(b.dtype.names)
+    if nfieldsa != nfieldsb:
+        print("number of fields don't match")
+        return False
+    for i in range(nfieldsa):
+        fielda = a.field(i)
+        fieldb = b.field(i)
+        if fielda.dtype.char == 'S':
+            fielda = decode_ascii(fielda)
+        if fieldb.dtype.char == 'S':
+            fieldb = decode_ascii(fieldb)
+        if (not isinstance(fielda, type(fieldb)) and not
+            isinstance(fieldb, type(fielda))):
+            print("type(fielda): ", type(fielda), " fielda: ", fielda)
+            print("type(fieldb): ", type(fieldb), " fieldb: ", fieldb)
+            print('field {0} type differs'.format(i))
+            return False
+        if len(fielda) and isinstance(fielda[0], np.floating):
+            if not comparefloats(fielda, fieldb):
+                print("fielda: ", fielda)
+                print("fieldb: ", fieldb)
+                print('field {0} differs'.format(i))
+                return False
+        elif (isinstance(fielda, fits.column._VLF) or
+              isinstance(fieldb, fits.column._VLF)):
+            for row in range(len(fielda)):
+                if np.any(fielda[row] != fieldb[row]):
+                    print('fielda[{0}]: {1}'.format(row, fielda[row]))
+                    print('fieldb[{0}]: {1}'.format(row, fieldb[row]))
+                    print('field {0} differs in row {1}'.format(i, row))
+        else:
+            if np.any(fielda != fieldb):
+                print("fielda: ", fielda)
+                print("fieldb: ", fieldb)
+                print('field {0} differs'.format(i))
+                return False
+    return True
