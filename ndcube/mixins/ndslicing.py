8513aa95b2aa71bbd7b85030884758383bf98682
Yash <yashrsharma44@gmail.com>
2019-06-27 16:01:44 +0530

    Added the following changes 1. Modified the docstring of `_slice`. 2. Added a note of the ordering of slicing that is being used.

--- ndcube/mixins/ndslicing.py
+++ ndcube/mixins/ndslicing.py
@@ -33,25 +33,36 @@ class NDCubeSlicingMixin(NDSlicingMixin):
         return super().__getitem__(item)
 
     def _slice(self, item):
-        """
-        Construct a set of keyword arguments to initialise a new (sliced)
+        """Construct a set of keyword arguments to initialise a new (sliced)
         instance of the class. This method is called in
         `astropy.nddata.mixins.NDSlicingMixin.__getitem__`.
 
         This method extends the `~astropy.nddata.mixins.NDSlicingMixin` method
-        to add support for ``missing_axes`` and ``extra_coords`` and overwrites
-        the astropy handling of wcs slicing.
+        to add support for  ``extra_coords`` and overwrites the astropy 
+        handling of wcs slicing.
+        
+        Parameters
+        ----------
+        item : slice
+            The slice passed to ``__getitem__``. Note that the item parameter corresponds 
+            to numpy ordering, keeping with the convention for NDCube.
+        
+        Returns
+        -------
+        dict :
+            Containing all the attributes after slicing - ready to
+            use them to create ``self.__class__.__init__(**kwargs)`` in
+            ``__getitem__``.
         """
 
         item = tuple(sanitize_slices(item, len(self.dimensions)))
         kwargs = super()._slice(item)
 
         # Store the original dimension of the wcs object before slicing
-        prev_dim = self.high_level_wcs.low_level_wcs.world_n_dim
+        prev_dim = self.wcs.world_n_dim
 
         # Sanitize the input arguments
         wcs = self._slice_wcs(item)
-        print('ITEM {0}'.format(item))
 
         # Set the kwargs values
         kwargs['wcs'] = wcs
@@ -59,8 +70,21 @@ class NDCubeSlicingMixin(NDSlicingMixin):
         return kwargs
 
     def _slice_wcs(self, item):
-
-        # Returns the sliced WCS object
+        """Helper function which returns the sliced WCS object
+        
+        Parameters
+        ----------
+        item : slice
+            The slice parameter for slicing WCS object. Note that it accepts the 
+            slice parameter in numpy order, and passes the slice parameter into
+            `SlicedLowLevelWCS` into numpy order, as `SlicedLowLevelWCS` currently 
+            takes the slice parameter in numpy ordering. 
+        
+        Returns
+        -------
+        SlicedLowLevelWCS object
+            The sliced WCS object.
+        """
         return SlicedLowLevelWCS(self.wcs, item)
 
 
