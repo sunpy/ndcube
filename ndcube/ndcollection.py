1c7518755285f4ae02b0a5445cbf39860a0d6cf5
DanRyanIrish <ryand5@tcd.ie>
2020-02-20 10:46:13 -0500

    Move sanitize_aligned_axes function to collection utils.

--- ndcube/ndcollection.py
+++ ndcube/ndcollection.py
@@ -9,7 +9,7 @@ __all__ = ["NDCollection"]
 
 #class NDCollection(collections.abc.Sequence):
 class NDCollection:
-    def __init__(self, data, keys=None, aligned_axes=None, dont_sanitize_aligned_axes=False):
+    def __init__(self, data, keys=None, aligned_axes=None, meta=None, dont_sanitize_aligned_axes=False):
         """
         A class for holding and manipulating a collection of aligned NDCube or NDCubeSequences.
 
@@ -31,6 +31,9 @@ class NDCollection:
             If elements are tuples of ints, then must be one tuple for every cube/sequence.
             Each element of each tuple gives the axes of each cube/sequence that are aligned.
             Default is all axes are aligned.
+
+        meta: `dict` (Optional)
+            General metadata for the overall collection.
 
         Example
         -------
@@ -54,6 +57,8 @@ class NDCollection:
         if len(keys) != len(data):
             raise ValueError("Data and keys inputs of different lengths.")
 
+        self.meta = meta
+
         n_cubes = len(data)
         self.data = dict(zip(keys, data))
         self.keys = tuple(keys)
@@ -75,7 +80,8 @@ class NDCollection:
                 self.n_aligned_axes = len(aligned_axes[0])
                 self.aligned_axes = dict(zip(keys, aligned_axes))
             else:
-                aligned_axes, self.n_aligned_axes = _sanitize_aligned_axes(data, aligned_axes, n_cubes)
+                aligned_axes, self.n_aligned_axes = collection_utils._sanitize_aligned_axes(
+                        data, aligned_axes, n_cubes)
                 self.aligned_axes = dict(zip(keys, aligned_axes))
 
     def __getitem__(self, item):
@@ -118,7 +124,8 @@ class NDCollection:
                 # Therefore the collection keys remain unchanged.
                 new_keys = self.keys
 
-            return self.__class__(new_data, keys=new_keys, aligned_axes=new_aligned_axes)
+            return self.__class__(new_data, keys=new_keys, aligned_axes=new_aligned_axes,
+                                  meta=self.meta, dont_sanitize_aligned_axes=True)
 
     def _generate_collection_getitems(self, item):
         # There are 3 supported cases of the slice item: int, slice, tuple of ints and/or slices.
@@ -200,71 +207,4 @@ Aligned world physical axis types: {aligned_axis_types}""".format(
     @property
     def aligned_world_axis_physical_types(self):
         return self.data[self.keys[0]].world_axis_physical_types
-
-
-def _sanitize_aligned_axes(data, aligned_axes, n_cubes):
-    aligned_axes_error_message = "aligned_axes must contain ints or " + \
-            "a tuple of ints for each element in data."
-    cube0_dims = data[0].dimensions
-    # If user entered a single int, convert to length 1 tuple of int.
-    if isinstance(aligned_axes, int):
-        aligned_axes = tuple(aligned_axes)
-    if not isinstance(aligned_axes, tuple):
-        raise ValueError(aligned_axes_error_message)
-    # Check type of each element.
-    axes_all_ints = all([isinstance(axis, int) for axis in aligned_axes])
-    axes_all_tuples = all([isinstance(axis, tuple) for axis in aligned_axes])
-    # If all elements are int, duplicate tuple so there is one for each cube.
-    if axes_all_ints is True:
-
-        n_aligned_axes = len(aligned_axes)
-        aligned_axes = tuple([aligned_axes for i in range(n_cubes)])
-
-    # If all elements are tuple, ensure there is a tuple for each cube and
-    # all elements of each sub-tuple are ints.
-    elif axes_all_tuples is True:
-        if len(aligned_axes) != n_cubes:
-            raise ValueError(
-                "aligned_axes must have a tuple for each element in data.")
-
-        n_aligned_axes = len(aligned_axes[0])
-
-        # Ensure all elements of sub-tuples are ints,
-        # each tuple has the same number of aligned axes,
-        # number of aligned axes are <= number of cube dimensions,
-        # and the dimensions of the aligned axes in each cube are the same.
-        subtuples_are_ints = [False] * n_cubes
-        aligned_axes_same_lengths = [False] * n_cubes
-        subtuple_types = [False] * n_aligned_axes
-        if not all([len(axes) == n_aligned_axes for axes in aligned_axes]):
-            raise ValueError("Each element in aligned_axes must have same length.")
-        for i in range(n_cubes):
-            # Check each cube has at least as many dimensions as there are aligned axes
-            # and that all cubes have enough dimensions to accommodate aligned axes.
-            n_cube_dims = len(data[i].dimensions)
-            max_aligned_axis = max(aligned_axes[i])
-            if n_cube_dims < max([max_aligned_axis, n_aligned_axes]):
-                raise ValueError(
-                    "Each cube in data must have at least as many axes as aligned axes " + \
-                    "and aligned axis numbers must be less than number of cube axes.\n" + \
-                    "Cube number: {0};\n".format(i) + \
-                    "Number of cube dimensions: {0};\n".format(n_cube_dims) + \
-                    "No. aligned axes: {0};\n".format(n_aligned_axes)  + \
-                    "Highest aligned axis: {0}".format(max_aligned_axis))
-            subtuple_types = [False] * n_aligned_axes
-            cube_lengths_equal = [False] * n_aligned_axes
-            for j, axis in enumerate(aligned_axes[i]):
-                subtuple_types[j] = isinstance(axis, (int, np.integer))
-                cube_lengths_equal[j] = data[i].dimensions[axis] == cube0_dims[axis]
-            subtuples_are_ints[i] = all(subtuple_types)
-            aligned_axes_same_lengths[i] = all(cube_lengths_equal)
-        if not all(subtuples_are_ints):
-            print([[type(axis) for axis in aligned_axis] for aligned_axis in aligned_axes], subtuple_types)
-            raise ValueError(aligned_axes_error_message)
-        if not all(aligned_axes_same_lengths):
-            raise ValueError("Aligned cube/sequence axes must be of same length.")
-    else:
-        raise ValueError(aligned_axes_error_message)
-
-    return aligned_axes, n_aligned_axes
 
--- ndcube/utils/collection.py
+++ ndcube/utils/collection.py
@@ -1,5 +1,84 @@
 import numpy as np
 
+def _sanitize_aligned_axes(data, aligned_axes, n_cubes):
+    """
+    Converts input aligned_axes to standard format.
+
+    aligned_axes can be supplied by the user in a few ways:
+    *. A tuple of tuples of ints, where each tuple corresponds to a cube
+    in the collection, and each int designates the an aligned axis in numpy order.
+    In this case, the axis represented by the 0th int in the 0th tuple is aligned
+    with the 0th int in the 1st tuple and so on.
+    *. A single tuple of ints if all aligned axes are in the same order.
+    *. A single int if only one axis is aligned and if the aligned axis in each cube
+    is in the same order.
+
+    """
+    aligned_axes_error_message = "aligned_axes must contain ints or " + \
+            "a tuple of ints for each element in data."
+    cube0_dims = data[0].dimensions
+    # If user entered a single int or string, convert to length 1 tuple of int.
+    if isinstance(aligned_axes, int):
+        aligned_axes = tuple(aligned_axes)
+    if not isinstance(aligned_axes, tuple):
+        raise ValueError(aligned_axes_error_message)
+    # Check type of each element.
+    axes_all_ints = all([isinstance(axis, int) for axis in aligned_axes])
+    axes_all_tuples = all([isinstance(axis, tuple) for axis in aligned_axes])
+    # If all elements are int, duplicate tuple so there is one for each cube.
+    if axes_all_ints is True:
+
+        n_aligned_axes = len(aligned_axes)
+        aligned_axes = tuple([aligned_axes for i in range(n_cubes)])
+
+    # If all elements are tuple, ensure there is a tuple for each cube and
+    # all elements of each sub-tuple are ints.
+    elif axes_all_tuples is True:
+        if len(aligned_axes) != n_cubes:
+            raise ValueError(
+                "aligned_axes must have a tuple for each element in data.")
+
+        n_aligned_axes = len(aligned_axes[0])
+
+        # Ensure all elements of sub-tuples are ints,
+        # each tuple has the same number of aligned axes,
+        # number of aligned axes are <= number of cube dimensions,
+        # and the dimensions of the aligned axes in each cube are the same.
+        subtuples_are_ints = [False] * n_cubes
+        aligned_axes_same_lengths = [False] * n_cubes
+        subtuple_types = [False] * n_aligned_axes
+        if not all([len(axes) == n_aligned_axes for axes in aligned_axes]):
+            raise ValueError("Each element in aligned_axes must have same length.")
+        for i in range(n_cubes):
+            # Check each cube has at least as many dimensions as there are aligned axes
+            # and that all cubes have enough dimensions to accommodate aligned axes.
+            n_cube_dims = len(data[i].dimensions)
+            max_aligned_axis = max(aligned_axes[i])
+            if n_cube_dims < max([max_aligned_axis, n_aligned_axes]):
+                raise ValueError(
+                    "Each cube in data must have at least as many axes as aligned axes " + \
+                    "and aligned axis numbers must be less than number of cube axes.\n" + \
+                    "Cube number: {0};\n".format(i) + \
+                    "Number of cube dimensions: {0};\n".format(n_cube_dims) + \
+                    "No. aligned axes: {0};\n".format(n_aligned_axes)  + \
+                    "Highest aligned axis: {0}".format(max_aligned_axis))
+            subtuple_types = [False] * n_aligned_axes
+            cube_lengths_equal = [False] * n_aligned_axes
+            for j, axis in enumerate(aligned_axes[i]):
+                subtuple_types[j] = isinstance(axis, (int, np.integer))
+                cube_lengths_equal[j] = data[i].dimensions[axis] == cube0_dims[axis]
+            subtuples_are_ints[i] = all(subtuple_types)
+            aligned_axes_same_lengths[i] = all(cube_lengths_equal)
+        if not all(subtuples_are_ints):
+            print([[type(axis) for axis in aligned_axis] for aligned_axis in aligned_axes], subtuple_types)
+            raise ValueError(aligned_axes_error_message)
+        if not all(aligned_axes_same_lengths):
+            raise ValueError("Aligned cube/sequence axes must be of same length.")
+    else:
+        raise ValueError(aligned_axes_error_message)
+
+    return aligned_axes, n_aligned_axes
+
 
 def _update_aligned_axes(drop_aligned_axes_indices, aligned_axes):
     # Remove dropped axes from aligned_axes.  MUST BE A BETTER WAY TO DO THIS.
@@ -20,6 +99,7 @@ def _update_aligned_axes(drop_aligned_axes_indices, aligned_axes):
         new_aligned_axes = aligned_axes
 
     return new_aligned_axes
+
 
 def slice_interval_is_1(item, axis_length):
     # Make start index numeric and positive.
@@ -56,3 +136,4 @@ def make_index_positive(index, axis_length):
     else:
         pos_index = index
     return pos_index
+
