ea94a9b39d1ff73e57d692e43f3ac661db36cded
DanRyanIrish <ryand5@tcd.ie>
2020-10-06 09:38:22 -0400

    Change NDCube method name to axis_world_coords_values.

--- ndcube/tests/test_ndcube.py
+++ ndcube/tests/test_ndcube.py
@@ -923,7 +923,7 @@ def test_crop_by_extra_coord(test_input, expected):
     ((cubem[0, 0], []), (u.Quantity([1.02e-09, 1.04e-09, 1.06e-09, 1.08e-09], unit=u.m),))
 ])
 def test_all_world_coords_with_input(test_input, expected):
-    all_coords = test_input[0].axis_world_coord_values(*test_input[1])
+    all_coords = test_input[0].axis_world_coords_values(*test_input[1])
     for i in range(len(all_coords)):
         np.testing.assert_allclose(all_coords[i].value, expected[i].value)
         assert all_coords[i].unit == expected[i].unit
@@ -934,7 +934,7 @@ def test_all_world_coords_with_input(test_input, expected):
     ((cubem, ['em']), (u.Quantity([1.01e-09, 1.03e-09, 1.05e-09, 1.07e-09, 1.09e-09], unit=u.m),))
 ])
 def test_all_world_coord_values_with_input_and_kwargs(test_input, expected):
-    all_coords = test_input[0].axis_world_coord_values(*test_input[1], **{"edges": True})
+    all_coords = test_input[0].axis_world_coords_values(*test_input[1], **{"edges": True})
     for i in range(len(all_coords)):
         np.testing.assert_allclose(all_coords[i].value, expected[i].value)
         assert all_coords[i].unit == expected[i].unit
@@ -952,8 +952,8 @@ def test_all_world_coord_values_with_input_and_kwargs(test_input, expected):
                         u.Quantity([1., 5.00000000e-01], unit=u.deg),
                         u.Quantity([1., 9.99987308e-01], unit=u.deg)))
 ])
-def test_axis_world_coord_values_without_input(test_input, expected):
-    all_coords = test_input.axis_world_coord_values()
+def test_axis_world_coords_values_without_input(test_input, expected):
+    all_coords = test_input.axis_world_coords_values()
     for i in range(len(all_coords)):
         assert_quantity_allclose(all_coords[i], expected[i])
         assert all_coords[i].unit == expected[i].unit
