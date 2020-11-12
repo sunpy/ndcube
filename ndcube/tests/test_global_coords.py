89074a1514436043e2db51bcf8a2e9876ff83b2d
Kris Stern <krisastern@gmail.com>
2020-11-12 23:47:14 +0800

    Apply suggestions from code review
    Co-authored-by: Stuart Mumford <stuart@cadair.com>

--- ndcube/tests/test_global_coords.py
+++ ndcube/tests/test_global_coords.py
@@ -3,17 +3,19 @@ import astropy.units as u
 import ndcube
 from ndcube.global_coords import GlobalCoords
 
-gc = GlobalCoords(ndcube)  # Initialize an empty GlobalCoords instance
+@pytest.fixture
+def global_coords(ndcube_3d_ln_lt_l):
+    return GlobalCoords(ndcube_3d_ln_lt_l)
 
 
-def test_some_basic_coordinates():
+def test_some_basic_coordinates(global_coords):
     coord1 = 1 * u.m
     coord2 = 2 * u.s
-    gc.add('name1', 'physical_type1', coord1)
-    gc.add('name2', 'physical_type2', coord2)
-    assert gc.names == ['name1', 'name2']
-    assert gc.physical_types == ['physical_type1', 'physical_type2']
-    assert gc.coords == [u.Quantity(1., u.m), u.Quantity(2., u.s)]
+    global_coords.add('name1', 'physical_type1', coord1)
+    global_coords.add('name2', 'physical_type2', coord2)
+    assert global_coords.names == ['name1', 'name2']
+    assert global_coords.physical_types == ['physical_type1', 'physical_type2']
+    assert global_coords.coords == [u.Quantity(1., u.m), u.Quantity(2., u.s)]
 
-    gc.remove('name2')
-    assert len(gc) == 1
+    global_coords.remove('name2')
+    assert len(global_coords) == 1
