1d202705ab73717d3994b1507de72da3396f5da1
Kris Stern <krisastern@gmail.com>
2020-11-12 23:16:03 +0800

    Fix tests for checking global coords

--- ndcube/global_coords/global_coords.py
+++ ndcube/global_coords/global_coords.py
@@ -1,4 +1,5 @@
 from collections.abc import Mapping
+from collections import OrderedDict
 
 __all__ = ['GlobalCoords']
 
@@ -15,7 +16,7 @@ class GlobalCoords(Mapping):
         self._ndcube = ndcube
 
         # Set values using the setters for validation
-        self._internal_coords = {}
+        self._internal_coords = OrderedDict()
 
     @property
     def _all_coords(self):
@@ -28,42 +29,55 @@ class GlobalCoords(Mapping):
         """
         Add a new coordinate to the collection.
         """
-        self.mapping[name] = [name, physical_type, coords]
+        self._internal_coords[str(name)] = (name, physical_type, coords)
 
     def remove(self, name):
         """
         Remove a coordinate from the collection
         """
-        del self.mapping[name]
+        del self._internal_coords[str(name)]
 
     @property
     def names(self):
         """
         A mutable list of all the names or keys.
         """
-        return [*self.mapping.keys()]
+        return list(self._internal_coords.keys())
 
     @property
     def physical_types(self):
         """
         A mutable list of all physical types, one per coordinate.
         """
-        return [*self.mapping[1]]
+        physical_types_list = []
+        for item in self._internal_coords.values():
+            physical_types_list.append(item[1])
+        return physical_types_list
+
+    @property
+    def coords(self):
+        """
+        A mutable list of all coords, one per coordinate.
+        """
+        coords_list = []
+        for item in self._internal_coords.values():
+            coords_list.append(item[2])
+        return coords_list
 
     def __getitem__(self, item):
         """
         Index the collection by a name.
         """
-        return self.mapping[item]
+        return self._internal_coords[item]
 
     def __iter__(self):
         """
         Iterate over the collection.
         """
-        return iter(self.mapping)
+        return iter(self._internal_coords)
 
     def __len__(self):
         """
         Establish the length of the collection.
         """
-        return len(self.mapping)
+        return len(self._internal_coords)
--- ndcube/tests/test_global_coords.py
+++ ndcube/tests/test_global_coords.py
@@ -1,7 +1,8 @@
 import astropy.units as u
+import ndcube
 from ndcube.global_coords import GlobalCoords
 
-gc = GlobalCoords()  # Initialze an empty GlobalCoords instance
+gc = GlobalCoords(ndcube)  # Initialize an empty GlobalCoords instance
 
 # Init
 coord1 = 1 * u.m
@@ -11,15 +12,9 @@ coord2 = 2 * u.s
 def test_some_basic_coordinates():
     gc.add('name1', 'physical_type1', coord1)
     gc.add('name2', 'physical_type2', coord2)
-    assert gc.names == ('name1', 'name2')
-    assert gc.physical_types == ('physical_type1', 'physical_type2')
-    assert gc.coords == (u.Quantity(1., u.m), u.Quantity(2., u.s))
-
-    gc_name1 = gc['name1']
-    assert gc_name1.names == 'name1'
-    assert gc_name1.coords == u.Quantity(1., u.m)
-
-    assert gc['name1'].physical_types == 'physical_type1'
+    assert gc.names == ['name1', 'name2']
+    assert gc.physical_types == ['physical_type1', 'physical_type2']
+    assert gc.coords == [u.Quantity(1., u.m), u.Quantity(2., u.s)]
 
     gc.remove('name2')
-    assert gc == gc_name1
+    assert len(gc) == 1
