0d40a96d5060e8bc1eb6c734d170f19efbadb738
Kris Stern <krisastern@gmail.com>
2020-11-12 18:47:52 +0800

    Revise PR according to reviewer suggestions

--- ndcube/global_coords/global_coords.py
+++ ndcube/global_coords/global_coords.py
@@ -1,6 +1,5 @@
+from collections import OrderedDict
 from collections.abc import Mapping
-
-from ndcube import NDCube
 
 __all__ = ['GlobalCoords']
 
@@ -9,68 +8,78 @@ class GlobalCoords(Mapping):
     """
     A structured representation of coordinate information applicable to a whole NDCube.
     """
-    def __init__(self, NDCube):
+    def __init__(self):
+        """
+        Init method.
+        """
         super().__init__()
+        self._internal_coords = OrderedDict()
 
-        # Set values using the setters for validation
-        self.mapping = {}
+    @property
+    def _all_coords(self):
+        """
+        Establish _all_coords as a property returning some _internal_coords.
+        """
+        return self._internal_coords
 
-    def add(self, name, physical_type):
+    def add(self, name, physical_type, coords):
         """
         Add a new coordinate to the collection.
         """
-        if len(self.mapping) > 1:
-            try:
-                self.mapping[name] = name
-                self.mapping[physical_type] = physical_type
-            except TypeError:
-                self.mapping[physical_type] = physical_type
-            else:
-                self.mapping[name] = name
+        if name in self._internal_coords.keys():
+            raise ValueError("coordinate with same name already exists: "
+                             f"{name}: {self._internal_coords[name]}")
+        self._internal_coords[name] = (physical_type, coords)
 
     def remove(self, name):
         """
         Remove a coordinate from the collection
         """
-        if len(self.mapping) > 1:
-            del self.mapping[name]
+        del self._internal_coords[name]
 
     @property
     def names(self):
         """
         A tuple of all the names or keys.
         """
-        if len(self.mapping) >= 1:
-            return [*self.mapping[name]]
+        return tuple(self._all_coords.keys())
 
     @property
     def physical_types(self):
         """
         A tuple of all physical types, one per coordinate.
         """
-        if len(self.mapping) >= 1:
-            return [*self.mapping[physical_type]]
+        return tuple(item[0] for item in self.values())
 
-    def keys(self):
-        """
-        A set-like of all names in this collection.
-        """
-        return set(self.mapping.keys())
+    def get_physical_type(self, name):
+        """Return the physical type of a specific coordinate."""
+        return self._all_coords[name][0]
 
-    def values(self):
-        """
-        A set-like of all values in this collection
-        """
-        return set(self.mapping.values())
+    def get_coord(self, name):
+        """Return value of a specific coordinate."""
+        return self._all_coords[name][1]
 
     def __getitem__(self, item):
         """
         Index the collection by a name.
         """
-        return self.mapping[item]
+        return self._all_coords[item]
 
     def __iter__(self):
-        return iter(self.mapping)
+        """
+        Iterate over the collection.
+        """
+        return iter(self._all_coords)
 
     def __len__(self):
-        return len(self.mapping)
+        """
+        Establish the length of the collection.
+        """
+        return len(self._all_coords)
+
+    def __str__(self):
+        names_and_coords = zip(self.names, self._all_coords.values())
+        return f"GlobalCoords({[(name, coord) for name, coord in names_and_coords]})"
+
+    def __repr__(self):
+        return f"{object.__repr__(self)}\n{str(self)}"
---
+++ ndcube/tests/test_global_coords.py
@@ -0,0 +1,99 @@
+import astropy.units as u
+import pytest
+
+from ndcube.global_coords import GlobalCoords
+
+
+@pytest.fixture
+def gc():
+    return GlobalCoords()
+
+
+def test_add(gc):
+    coord1 = 1 * u.m
+    coord2 = 2 * u.s
+    gc.add('name1', 'physical_type1', coord1)
+    gc.add('name2', 'physical_type2', coord2)
+    assert gc.names == ('name1', 'name2')
+    assert gc.physical_types == ('physical_type1', 'physical_type2')
+
+
+def test_remove(gc):
+    coord1 = 1 * u.m
+    coord2 = 2 * u.s
+    gc.add('name1', 'physical_type1', coord1)
+    gc.add('name2', 'physical_type2', coord2)
+    gc.remove('name2')
+    assert len(gc) == 1
+    assert gc.names == ('name1',)
+    assert gc.physical_types == ('physical_type1',)
+
+
+def test_overwrite(gc):
+    with pytest.raises(ValueError):
+        coord1 = 1 * u.m
+        coord2 = 2 * u.s
+        gc.add('name1', 'physical_type1', coord1)
+        gc.add('name1', 'physical_type2', coord2)
+
+
+def test_iterating(gc):
+    coord1 = 1 * u.m
+    coord2 = 2 * u.s
+    gc.add('name1', 'physical_type1', coord1)
+    gc.add('name2', 'physical_type2', coord2)
+    for i, gc_item in enumerate(gc):
+        if i == 0:
+            assert gc_item == 'name1'
+        if i == 1:
+            assert gc_item == 'name2'
+
+
+def test_slicing(gc):
+    coord1 = 1 * u.m
+    gc.add('name1', 'physical_type1', coord1)
+    assert gc['name1'] == ('physical_type1', u.Quantity(1., u.m))
+
+
+def test_names(gc):
+    coord1 = 1 * u.m
+    coord2 = 2 * u.s
+    gc.add('name1', 'physical_type1', coord1)
+    gc.add('name2', 'physical_type2', coord2)
+    assert gc.names == ('name1', 'name2')
+
+
+def test_physical_types(gc):
+    coord1 = 1 * u.m
+    coord2 = 2 * u.s
+    gc.add('name1', 'physical_type1', coord1)
+    gc.add('name2', 'physical_type2', coord2)
+    assert gc.physical_types == ('physical_type1', 'physical_type2')
+
+
+def test_len(gc):
+    coord1 = 1 * u.m
+    coord2 = 2 * u.s
+    gc.add('name1', 'physical_type1', coord1)
+    gc.add('name2', 'physical_type2', coord2)
+    assert len(gc) == 2
+
+
+def test_get_coord(gc):
+    coord1 = 1 * u.m
+    gc.add('name1', 'physical_type1', coord1)
+    gc.add('name2', 'physical_type2', coord2)
+    value_list = list(gc.values())
+    for i, element in enumerate(value_list):
+        if i == 0:
+            assert element[1] == coord1
+        if i == 1:
+            assert element[1] == coord2
+
+    assert gc.get_coord('name1') == coord1
+
+
+def test_get_physical_type(gc):
+    coord1 = 1 * u.m
+    gc.add('name1', 'physical_type1', coord1)
+    assert gc.get_physical_type('name1') == 'physical_type1'
