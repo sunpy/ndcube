d294bef285192c9faa0136a0ec221b9d5dc03b21
Kris Stern <krisastern@gmail.com>
2020-11-12 23:19:21 +0800

    Move definitions from root of file to test proper

--- ndcube/tests/test_global_coords.py
+++ ndcube/tests/test_global_coords.py
@@ -4,12 +4,10 @@ from ndcube.global_coords import GlobalCoords
 
 gc = GlobalCoords(ndcube)  # Initialize an empty GlobalCoords instance
 
-# Init
-coord1 = 1 * u.m
-coord2 = 2 * u.s
-
 
 def test_some_basic_coordinates():
+    coord1 = 1 * u.m
+    coord2 = 2 * u.s
     gc.add('name1', 'physical_type1', coord1)
     gc.add('name2', 'physical_type2', coord2)
     assert gc.names == ['name1', 'name2']
