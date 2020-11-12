4a37f0a94620091022fc34b6a4a56340d02fb66e
Kris Stern <krisastern@gmail.com>
2020-11-12 22:11:15 +0800

    Add testing module for global_coords

---
+++ ndcube/tests/test_global_coords.py
@@ -0,0 +1,25 @@
+import astropy.units as u
+from ndcube.global_coords import GlobalCoords
+
+gc = GlobalCoords()  # Initialze an empty GlobalCoords instance
+
+# Init
+coord1 = 1 * u.m
+coord2 = 2 * u.s
+
+
+def test_some_basic_coordinates():
+    gc.add('name1', 'physical_type1', coord1)
+    gc.add('name2', 'physical_type2', coord2)
+    assert gc.names == ('name1', 'name2')
+    assert gc.physical_types == ('physical_type1', 'physical_type2')
+    assert gc.coords == (<Quantity 1. m>, <Quantity 2. s>)
+
+    gc_name1 = gc['name1']
+    assert gc_name1.names == 'name1'
+    assert gc_name1.coords == <Quantity 1. m>
+
+    assert gc['name1'].physical_types == 'physical_type1'
+
+    gc.remove('name2')
+    assert gc == gc_name1
