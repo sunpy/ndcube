23b52e7b6675576faec6aacc17317178400c29e2
Kris Stern <krisastern@gmail.com>
2020-11-13 18:01:43 +0800

    Update test_global_coords to remove coords

--- ndcube/tests/test_global_coords.py
+++ ndcube/tests/test_global_coords.py
@@ -14,9 +14,8 @@ def test_some_basic_coordinates(global_coords):
     coord2 = 2 * u.s
     global_coords.add('name1', 'physical_type1', coord1)
     global_coords.add('name2', 'physical_type2', coord2)
-    assert global_coords.names == ['name1', 'name2']
-    assert global_coords.physical_types == ['physical_type1', 'physical_type2']
-    assert global_coords.coords == [u.Quantity(1., u.m), u.Quantity(2., u.s)]
+    assert global_coords.names == ('name1', 'name2')
+    assert global_coords.physical_types == ('physical_type1', 'physical_type2')
 
     global_coords.remove('name2')
     assert len(global_coords) == 1
