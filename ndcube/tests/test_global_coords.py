ed35e7d0d3336644bb68f69fcbe7e1ba5d96ec20
Stuart Mumford <stuart@cadair.com>
2021-03-09 12:13:06 +0000

    Add some tests for hypothetical froody WCSes

--- ndcube/tests/test_global_coords.py
+++ ndcube/tests/test_global_coords.py
@@ -3,8 +3,11 @@ import numpy as np
 import pytest
 from astropy.coordinates import SkyCoord
 from astropy.coordinates.spectral_coordinate import SpectralCoord
+from astropy.wcs.wcsapi.high_level_api import HighLevelWCSMixin
+from astropy.wcs.wcsapi.low_level_api import BaseLowLevelWCS
 
 from ndcube.global_coords import GlobalCoords
+from ndcube.ndcube import NDCube
 
 
 @pytest.fixture
@@ -117,3 +120,117 @@ def test_dropped_to_global_ec_gwcs_fail(ndcube_4d_extra_coords):
     assert isinstance(gc["helioprojective"], SkyCoord)
     assert isinstance(gc["time"], u.Quantity)
     assert isinstance(gc["hello"], u.Quantity)
+
+
+class SerializedWCS(BaseLowLevelWCS, HighLevelWCSMixin):
+    """
+    WCS with serialized classes
+    """
+
+    def pixel_to_world_values(self, *pixel_arrays):
+        return [np.asarray(pix) * 2 for pix in pixel_arrays]
+
+    def world_to_pixel_values(self, *world_arrays):
+        return [np.asarray(world) / 2 for world in world_arrays]
+
+    @property
+    def serialized_classes(self):
+        return True
+
+    @property
+    def pixel_n_dim(self):
+        return 2
+
+    @property
+    def world_n_dim(self):
+        return 2
+
+    @property
+    def axis_correlation_matrix(self):
+        return np.identity(2)
+
+    @property
+    def world_axis_physical_types(self):
+        return ['pos.eq.ra', 'pos.eq.dec']
+
+    @property
+    def world_axis_units(self):
+        return ['deg', 'deg']
+
+    @property
+    def world_axis_object_components(self):
+        return [('test', 0, 'value'), ('test2', 0, 'value')]
+
+    @property
+    def world_axis_object_classes(self):
+        return {'test': ('astropy.units.Quantity', (),
+                         {'unit': ('astropy.units.Unit', ('deg',), {})}),
+                'test2': ('astropy.units.Quantity', (),
+                          {'unit': ('astropy.units.Unit', ('deg',), {})})}
+
+
+def test_serialized_classes():
+    ndc = NDCube(np.zeros((10, 10)), wcs=SerializedWCS())
+    assert u.allclose(ndc[0, :].global_coords["pos.eq.dec"], 0*u.deg)
+
+
+class MultiCoord(list):
+    def __init__(self, *args):
+        super().__init__(args)
+
+    @property
+    def value(self):
+        return tuple(x.value() for x in self)
+
+
+class MultiCoordWCS(BaseLowLevelWCS, HighLevelWCSMixin):
+    """
+    WCS with serialized classes
+    """
+
+    def pixel_to_world_values(self, *pixel_arrays):
+        return [np.asarray(pix) * 2 for pix in pixel_arrays]
+
+    def world_to_pixel_values(self, *world_arrays):
+        return [np.asarray(world) / 2 for world in world_arrays]
+
+    @property
+    def pixel_n_dim(self):
+        return 3
+
+    @property
+    def world_n_dim(self):
+        return 3
+
+    @property
+    def axis_correlation_matrix(self):
+        return np.array([[1, 1, 0],
+                         [1, 1, 0],
+                         [0, 0, 1]])
+
+    @property
+    def world_axis_physical_types(self):
+        return ['pos.eq.ra', 'pos.eq.dec', 'pos.distance.x']
+
+    @property
+    def world_axis_units(self):
+        return ['deg', 'deg', 'm']
+
+    @property
+    def world_axis_object_components(self):
+        return [('test', 0, 'value'),
+                ('test', 1, 'value'),
+                ('distance', 0, 'value')]
+
+    @property
+    def world_axis_object_classes(self):
+        return {'test': (MultiCoord, (), {}),
+                'distance': (u.Quantity, (), {'unit': 'm'})}
+
+
+def test_non_skycoord_multi_object():
+    ndc = NDCube(np.zeros((10, 20, 30)), wcs=MultiCoordWCS())
+    gc = ndc[:, 0, 0].global_coords
+    assert len(gc._all_coords) == 1
+    assert gc["pos.eq.ra"] == gc["pos.eq.dec"] == gc[("pos.eq.ra", "pos.eq.dec")] == [0, 0]
+    assert not set(gc.keys()).difference({("pos.eq.ra", "pos.eq.dec")})
