53357e1c97a600cd6ba151872740b00820240611
Adwait Bhope <adwaitbhope@gmail.com>
2021-06-28 10:00:48 +0530

    Handle return_footprint

--- ndcube/ndcube.py
+++ ndcube/ndcube.py
@@ -689,15 +689,24 @@ class NDCubeBase(NDCubeSlicingMixin, NDCubeABC):
         if not utils.wcs.compare_wcs_physical_types(self.wcs, target_wcs):
             raise('Given target_wcs is not compatible with this NDCube.')
 
-        resampled_data, footprint = reproject_interp(self, output_projection=target_wcs,
-                                                     shape_out=shape_out, order=order,
-                                                     output_array=output_array,
-                                                     return_footprint=return_footprint)
+        if return_footprint:
+            resampled_data, footprint = reproject_interp(self, output_projection=target_wcs,
+                                                         shape_out=shape_out, order=order,
+                                                         output_array=output_array,
+                                                         return_footprint=return_footprint)
+        else:
+            resampled_data = reproject_interp(self, output_projection=target_wcs,
+                                              shape_out=shape_out, order=order,
+                                              output_array=output_array,
+                                              return_footprint=return_footprint)
 
         resampled_cube = NDCube(resampled_data, wcs=target_wcs, meta=deepcopy(self.meta))
         resampled_cube._global_coords = deepcopy(self.global_coords)
 
-        return resampled_cube, footprint
+        if return_footprint:
+            return resampled_cube, footprint
+
+        return resampled_cube
 
 
 class NDCube(NDCubeBase, astropy.nddata.NDArithmeticMixin):
