201fc0abff106a38adfb8f3faa413cd705affcd4
Adwait Bhope <adwaitbhope@gmail.com>
2021-07-12 20:03:59 +0530

    Update ndcube/ndcube.py
    Co-authored-by: Stuart Mumford <stuart@cadair.com>

--- ndcube/ndcube.py
+++ ndcube/ndcube.py
@@ -663,8 +663,8 @@ class NDCubeBase(NDCubeSlicingMixin, NDCubeABC):
             The WCS object on which the ``NDCube`` is to be reprojected.
 
         shape_out: `tuple`, optional
-            The shape of the output data. If not specified, `pixel_shape` attribute (if available)
-            from the low level API of the `target_wcs` is used.
+            The shape of the output data. If not specified, `~astropy.wcs.wcsapi.BaseLowLevelWCS.pixel_shape` attribute (if available)
+            from the low level API of the ``target_wcs`` is used.
 
         order: `int or str`
             The order of the interpolation. This can be any of: 'nearest-neighbour', 'bilinear',
