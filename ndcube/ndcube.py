2212e894e67aa97d3a8d4e4787de167d4d4c04a7
Stuart Mumford <stuart@cadair.com>
2021-07-20 11:41:08 +0100

    Write docstring for _crop_from_points

--- ndcube/ndcube.py
+++ ndcube/ndcube.py
@@ -622,6 +622,24 @@ class NDCubeBase(NDCubeSlicingMixin, NDCubeABC):
         return tuple(itertools.product(*zip(lower_corner_values, upper_corner_values)))
 
     def _crop_from_points(self, *world_points_values, wcs):
+        """
+        Crop the cube such that all the specified world points fall within it.
+
+        Parameters
+        ----------
+        world_points_values
+            The world coordinates in wcsapi "values" form (i.e. arrays /
+            floats), for however many world points should be contained in the
+            output cube.
+
+        wcs : `~astropy.wcs.wcsapi.BaseHighLevelWCS`, `~astropy.wcs.wcsapi.BaseLowLevelWCS`
+            The WCS to use to convert the world coordinates to array indices.
+
+        Returns
+        -------
+        new_cube
+            A new ``NDCube`` instance.
+        """
         if isinstance(wcs, BaseHighLevelWCS):
             wcs = wcs.low_level_wcs
 
