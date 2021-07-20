6994ecc943998e83b39dc35d71cc9e8a38a414de
Adwait Bhope <adwaitbhope@gmail.com>
2021-07-20 12:35:30 +0530

    Break a test into two

--- ndcube/tests/test_ndcube.py
+++ ndcube/tests/test_ndcube.py
@@ -522,7 +522,7 @@ def test_initialize_from_ndcube(ndcube_3d_l_ln_lt_ectime):
     assert ec is not ec3
 
 
-def test_resampling(ndcube_4d_ln_l_t_lt, wcs_4d_lt_t_l_ln, wcs_3d_lt_ln_l):
+def test_reproject(ndcube_4d_ln_l_t_lt, wcs_4d_lt_t_l_ln, wcs_3d_lt_ln_l):
     target_wcs_header = wcs_4d_lt_t_l_ln.low_level_wcs.to_header()
     target_wcs_header['CDELT3'] = 0.1   # original value = 0.2
     target_wcs = astropy.wcs.WCS(header=target_wcs_header)
@@ -533,12 +533,15 @@ def test_resampling(ndcube_4d_ln_l_t_lt, wcs_4d_lt_t_l_ln, wcs_3d_lt_ln_l):
     assert ndcube_4d_ln_l_t_lt.data.shape == (5, 10, 12, 8)
     assert resampled_cube.data.shape == (5, 20, 12, 8)
 
-    # Raises an exception if given target_wcs is not compatible with the NDCube
+
+def test_reproject_invalid_wcs(ndcube_4d_ln_l_t_lt, wcs_3d_lt_ln_l):
+    shape_out = (5, 20, 12, 8)
+
     with pytest.raises(Exception):
         _ = ndcube_4d_ln_l_t_lt.reproject(wcs_3d_lt_ln_l, shape_out)
 
 
-def test_resampling_return_footprint(ndcube_4d_ln_l_t_lt, wcs_4d_lt_t_l_ln):
+def test_reproject_return_footprint(ndcube_4d_ln_l_t_lt, wcs_4d_lt_t_l_ln):
     target_wcs_header = wcs_4d_lt_t_l_ln.low_level_wcs.to_header()
     target_wcs_header['CDELT3'] = 0.1   # original value = 0.2
     target_wcs = astropy.wcs.WCS(header=target_wcs_header)
@@ -552,7 +555,7 @@ def test_resampling_return_footprint(ndcube_4d_ln_l_t_lt, wcs_4d_lt_t_l_ln):
     assert footprint.shape == (5, 20, 12, 8)
 
 
-def test_resampling_shape_out(ndcube_4d_ln_l_t_lt, wcs_4d_lt_t_l_ln):
+def test_reproject_shape_out(ndcube_4d_ln_l_t_lt, wcs_4d_lt_t_l_ln):
     # should raise an exception when neither shape_out is specified nor
     # target_wcs has the pixel_shape or array_shape attribute
     wcs_4d_lt_t_l_ln.pixel_shape = None
