8651d31d4e384b0ec623ab3e4660588b2eb49c57
Adwait Bhope <adwaitbhope@gmail.com>
2021-07-29 10:24:45 +0530

    Add tests for reproject adaptive

--- ndcube/tests/test_ndcube.py
+++ ndcube/tests/test_ndcube.py
@@ -630,3 +630,18 @@ def test_wcs_type_after_init(ndcube_3d_ln_lt_l, wcs_3d_l_lt_ln):
     cube = NDCube(ndcube_3d_ln_lt_l.data[slices], low_level_wcs)
     # Check the WCS has been converted to high level but NDCube init.
     assert isinstance(cube.wcs, BaseHighLevelWCS)
+
+
+def test_reproject_adaptive(ndcube_2d_ln_lt, wcs_2d_lt_ln):
+    shape_out = (10, 12)
+    resampled_cube = ndcube_2d_ln_lt.reproject_to(wcs_2d_lt_ln, algorithm='adaptive',
+                                                  shape_out=shape_out)
+
+    assert ndcube_2d_ln_lt.data.shape == (10, 12)
+    assert resampled_cube.data.shape == (10, 12)
+
+
+def test_reproject_adaptive_incompatible_wcs(ndcube_4d_ln_l_t_lt, wcs_4d_lt_t_l_ln):
+    with pytest.raises(ValueError):
+        _ = ndcube_4d_ln_l_t_lt.reproject_to(wcs_4d_lt_t_l_ln, algorithm='adaptive',
+                                             shape_out=(5, 10, 12, 8))
