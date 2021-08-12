baf5d9d63ff1d807a8456f877c78bcb327f4621a
Derek Homeier <dhomeie@gwdg.de>
2021-08-12 19:21:28 +0200

    Some additional ndcube.data info in NDCubeBase.__str__

--- ndcube/ndcube.py
+++ ndcube/ndcube.py
@@ -667,7 +667,8 @@ class NDCubeBase(NDCubeSlicingMixin, NDCubeABC):
                 NDCube
                 ------
                 Dimensions: {self.dimensions}
-                Physical Types of Axes: {self.array_axis_physical_types}""")
+                Physical Types of Axes: {self.array_axis_physical_types}
+                Data: Array{self.data.shape} [{self.unit}] '{self.data.dtype}'""")
 
     def __repr__(self):
         return f"{object.__repr__(self)}\n{str(self)}"
