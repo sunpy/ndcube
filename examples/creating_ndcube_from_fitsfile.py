3690806de578be78ad1e39c34860ff0715b4cbd8
hayesla <hayesla@tcd.ie>
2022-08-26 18:47:29 +0100

    adding space

--- examples/creating_ndcube_from_fitsfile.py
+++ examples/creating_ndcube_from_fitsfile.py
@@ -15,35 +15,39 @@ from astropy.wcs import WCS
 from ndcube import NDCube
 
 ##############################################################################
-# We first download the example file that we will use here to show how to create an NDCube from data stored in a FITS file. 
+# We first download the example file that we will use here to show how to create an
+# NDCube from data stored in a FITS file.
 # Here we are using the example file from astropy.
 
 image_file = get_pkg_data_filename('tutorials/FITS-images/HorseHead.fits')
 
 ###########################################################################
 # Lets extract the image data and the header information from the FITS file.
-# This can be achived by using the functionality within `astropy.io` 
+# This can be achived by using the functionality within `astropy.io`
 # Here the the image information is located in the Primary HDU (extension 0).
 image_data = fits.getdata(image_file)
 image_header = fits.getheader(image_file)
 
 
 ##########################################################################
-# To create a NDCube object, we need both the data array and a WCS object (e.g. an astropy.wcs.WCS). 
+# To create a NDCube object, we need both the data array and a WCS object (e.g. an astropy.wcs.WCS).
 # Here the data WCS information is within the header, which we can pass to `astropy.wcs.WCS()` to create a WCS object.
 
 example_ndcube = NDCube(image_data, WCS(image_header))
 
 ##########################################################################
-# Now we have created an NDCube from this data. 
+# Now we have created an NDCube from this data.
 # We can inspect the NDCube, such as the WCS
+
 
 example_ndcube.wcs
 
-# and we can also inspect the dimensions 
+##########################################################################
+# and we can also inspect the dimensions
 
 example_ndcube.dimensions
 
+##########################################################################
 # We can also quickly visualize the data using the NDCube.plot method
 
 example_ndcube.plot()
