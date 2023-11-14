47ffb62dbbbf37d33af52af0173d28aef95e0115
DanRyanIrish <ryand5@tcd.ie>
2023-11-14 16:29:18 +0100

    rename wcs_tools.py to tools.py

--- ndcube/wcs/tests/test_wcs_tools.py
+++ ndcube/wcs/tests/test_wcs_tools.py
@@ -4,7 +4,7 @@ from astropy.wcs import WCS
 from astropy.wcs.wcsapi import SlicedLowLevelWCS
 from numpy.testing import assert_array_almost_equal, assert_array_equal
 
-from ndcube.wcs.wcs_tools import unwrap_wcs_to_fitswcs
+from ndcube.wcs.tools import unwrap_wcs_to_fitswcs
 from ndcube.wcs.wrappers import ResampledLowLevelWCS
 
 
