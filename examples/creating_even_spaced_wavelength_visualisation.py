cf97140adc9ffd20a68c403c91f69029c688325d
DanRyanIrish <ryand5@tcd.ie>
2024-04-25 15:35:34 +0200

    Apply suggestions from code review

--- examples/creating_even_spaced_wavelength_visualisation.py
+++ examples/creating_even_spaced_wavelength_visualisation.py
@@ -1,13 +1,15 @@
 """
 =================
-Creating an even spaced wavelength dimension in AIA
+Combining a Celestial WCS with a Wavelength Axis and Building a Spectral-Image Cube
 =================
 
-This example shows you how to create an illustration of the changing intensity across a
-band of wavelength observations. The result is an interactive figure of the observation.
+This example shows how to add an arbitrarily spaced wavelength dimension to a
+celestial WCS.  It then combines the new WCS with a stack of AIA images at
+different wavelengths, and produces an interactive figure of the observations.
 
-The example uses `sunpy.Fido` to retrieve the data and `sunpy.Map` handle the data and its meta.
-`NDCube` is used to stack the images into a cube in order the adjustment through wavelength.
+The example uses `sunpy.Fido` to retrieve a bunch of AIA files for different filters
+and `sunpy.Map` to read the files.  `NDCube` is used to stack the images into a
+spectral-image cube with the new 3D WCS.
 
 """
 import matplotlib.pyplot as plt
@@ -23,25 +25,27 @@ from ndcube.extra_coords import QuantityTableCoordinate
 from ndcube.wcs.wrappers import CompoundLowLevelWCS
 
 #############################################################################
-# Use SunPy's Fido to search for the desired data and then load it into map to utilise the benefits of WCS.
-# `sequence=True` allows a series of maps to be handled.
+# Use SunPy's Fido to search for the desired data and then read the data files with sunpy Map.
+# `sequence=True` causes a sequence of maps to be returned, one for each image file.
 aia_files = Fido.fetch(Fido.search(a.Time("2023/01/01", "2023/01/01 00:00:11"), a.Instrument.aia))
 maps = sunpy.map.Map(aia_files, sequence=True)
 
 #############################################################################
-# Access the map sequenece on the map object and ensure they are sorted by wavelength.
+# Sort the maps in the sequence in order of wavelength.
 maps.maps = list(sorted(maps.maps, key=lambda m: m.wavelength))
 
 #############################################################################
-# Utilise the AstroPy Quantity functionality to create a set of quantites matching the wavelengths of the observations.
-# These are then utilised to create a lookup-table type object via `QuantityTableCoordinate`, creating a corresponding wavelength
-# for each pixel in the wavelength (think depth) dimension. Finally, this is then passed to CompoundLowLevelWCS, which constructs the final cube orientation.
+# Create an AstroPy Quantity of the wavelengths of the images and use it to build a
+# 1-D lookup-table WCS via `QuantityTableCoordinate`.
+# This is then combined with the celestial WCS into a single 3-D WCS
+# via CompoundLowLevelWCS.
 waves = u.Quantity([m.wavelength for m in maps])
 wave_wcs = QuantityTableCoordinate(waves, physical_types="em.wl", names="wavelength").wcs
-cube_wcs = CompoundLowLevelWCS(wave_wcs, maps[0].wcs)
+cube_wcs = CompoundLowLevelWCS(wave_wcs, maps[0].wcs)  # We have put the WCS wavelength axis first. Therefore, the last axis of the associated spectral-image data array will have to be last.
 
 #############################################################################
-# Lets plot some stuff!
+# Combine the new 3-D WCS with the stack of AIA images via NDCube.
 my_cube = NDCube(maps.as_array(), wcs=cube_wcs)
+# Produce an interactive plot of the spectral-image stack.
 my_cube.plot(plot_axes=['y', 'x', None])
 plt.show()
