86dc36699d3f874b988142a1727b15aec1daac57
DanRyanIrish <ryand5@tcd.ie>
2024-05-07 17:13:47 +0200

    Reverting unintented changes to __init__.py

--- ndcube/__init__.py
+++ ndcube/__init__.py
@@ -7,23 +7,6 @@ A base package for multi-dimensional contiguous and non-contiguous coordinate-aw
 * Homepage: https://github.com/sunpy/ndcube
 * Documentation: https://docs.sunpy.org/projects/ndcube/
 """
-import sys
-
-# Enforce Python version check during package import.
-# Must be done before any ndcube imports
-__minimum_python_version__ = "3.7"
-
-
-class UnsupportedPythonError(Exception):
-    """Running on an unsupported version of Python."""
-
-
-if sys.version_info < tuple(int(val) for val in __minimum_python_version__.split('.')):
-    # This has to be .format to keep backwards compatibly.
-    raise UnsupportedPythonError(
-        "sunpy does not support Python < {}".format(__minimum_python_version__))
-
-
 from .extra_coords.extra_coords import ExtraCoords, ExtraCoordsABC
 from .global_coords import GlobalCoords, GlobalCoordsABC
 from .meta import Meta
