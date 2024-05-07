fd3864d26a48f4887df16e309ecac1ccf9c13999
Shane Maloney <shane.maloney@dias.ie>
2024-05-07 11:24:18 +0100

    Use CompoundLowLevelWCS instead of ExtraCoords

--- examples/creating_a_gwcs_from_quantities.py
+++ examples/creating_a_gwcs_from_quantities.py
@@ -8,12 +8,14 @@ This example shows to create a gWCS from quantities in this example time and ene
 """
 
 import numpy as np
+from matplotlib import pyplot as plt
 
 import astropy.units as u
 from astropy.time import Time
 
-from ndcube import ExtraCoords, NDCube
+from ndcube import NDCube
 from ndcube.extra_coords import QuantityTableCoordinate, TimeTableCoordinate
+from ndcube.wcs.wrappers import CompoundLowLevelWCS
 
 ##############################################################################
 # First we create our coordinate arrays.
@@ -27,24 +29,27 @@ time = Time('2020-01-01 00:00:00') + np.arange(9)*u.s
 # `~ndcube.extra_coords.table_coord.TimeTableCoordinate` to create table coordinates.
 
 energy_coord = QuantityTableCoordinate(energy, names='energy', physical_types='em.energy')
-energy_coord
+print(energy_coord)
 
 time_coord = TimeTableCoordinate(time, names='time', physical_types='time')
-time_coord
+print(time_coord)
 
 ##############################################################################
-# Create new `~ndcube.extra_coords.extra_coords.ExtraCoords` instance and add the previously created table coordinates
-# and extract the gWCS.
+# Create new `~ndcube.wcs.wrappers.compound_wcs.CompoundLowLevelWCS` instance using the previously created table
+# coordinates WCSs (note the ordering).
 
-extra_coords = ExtraCoords()
-extra_coords.add('energy', array_dimension=1, lookup_table=energy_coord)
-extra_coords.add('time', array_dimension=0, lookup_table=time_coord)
-wcs = extra_coords.wcs
-wcs
+wcs = CompoundLowLevelWCS(time_coord.wcs, energy_coord.wcs, )
+print(wcs)
 
 ##############################################################################
 # Finally we make a data array and create new `~ndcube.NDCube` with this data and the gWCS we just created.
 
 data = np.random.rand(len(time), len(energy))
 cube = NDCube(data=data, wcs=wcs)
-cube
+print(cube)
+
+##############################################################################
+# Make a plot
+
+cube.plot()
+plt.show()
