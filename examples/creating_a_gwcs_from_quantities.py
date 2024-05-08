a6e6b31bb4cbb2904634e0734191f513c598f554
Shane Maloney <maloneys@tcd.ie>
2024-05-08 10:28:42 +0100

    Apply suggestions from code review
    Co-authored-by: Nabil Freij <nabil.freij@gmail.com>

--- examples/creating_a_gwcs_from_quantities.py
+++ examples/creating_a_gwcs_from_quantities.py
@@ -1,12 +1,10 @@
 """
 ===============================================
-How to create an gWCS from quantities and times
+How to create an GWCS from quantities and times
 ===============================================
 
-This example shows to create a gWCS from quantities in this example time and energy.
-
+This example shows how to create a GWCS from astropy quantities.
 """
-
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -18,13 +16,13 @@ from ndcube.extra_coords import QuantityTableCoordinate, TimeTableCoordinate
 from ndcube.wcs.wrappers import CompoundLowLevelWCS
 
 ##############################################################################
-# First we create our coordinate arrays.
+# We aim to create coordinates that are focused around time and energies using astropy quantities.
 
 energy = np.arange(10) * u.keV
 time = Time('2020-01-01 00:00:00') + np.arange(9)*u.s
 
 ##############################################################################
-# Then we use use
+# Then, we need to turn these into lookup tables using
 # `~ndcube.extra_coords.table_coord.QuantityTableCoordinate` and
 # `~ndcube.extra_coords.table_coord.TimeTableCoordinate` to create table coordinates.
 
@@ -35,21 +33,24 @@ time_coord = TimeTableCoordinate(time, names='time', physical_types='time')
 print(time_coord)
 
 ##############################################################################
-# Create new `~ndcube.wcs.wrappers.compound_wcs.CompoundLowLevelWCS` instance using the previously created table
-# coordinates WCSs (note the ordering).
+# Now we need to create a `~ndcube.wcs.wrappers.compound_wcs.CompoundLowLevelWCS` instance
+# using the previously created table coordinates.
+# Please note the ordering, it is important to make sure you are assigning them
+# in the correct order. 
 
 wcs = CompoundLowLevelWCS(time_coord.wcs, energy_coord.wcs, )
 print(wcs)
 
 ##############################################################################
-# Finally we make a data array and create new `~ndcube.NDCube` with this data and the gWCS we just created.
+# Now, we have all of the pieces required to construct a `~ndcube.NDCube` with this data and the GWCS we just created.
 
 data = np.random.rand(len(time), len(energy))
 cube = NDCube(data=data, wcs=wcs)
 print(cube)
 
 ##############################################################################
-# Make a plot
+# Finally, we will plot the cube.
 
 cube.plot()
+
 plt.show()
