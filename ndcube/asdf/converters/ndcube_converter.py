6e619fc5c004952a942b007ec4dfa1641c68ac8a
ViciousEagle03 <piyushsharma04321@gmail.com>
2024-05-23 04:16:00 +0530

    Remove  method to remove any delay and

--- ndcube/asdf/converters/ndcube_converter.py
+++ ndcube/asdf/converters/ndcube_converter.py
@@ -4,18 +4,7 @@ import numpy as np
 
 class NDCubeConverter(Converter):
     tags = ["tag:sunpy.org:ndcube/ndcube/NDCube-*"]
-
-    @property
-    def types(self):
-        from ndcube.ndcube import NDCube
-        return [NDCube]
-
-    def select_tag(self, obj, tags, ctx):
-        # Sort the tags in reverse alphabetical order and pick the first (i.e.
-        # the one with the highest version). This assumes that all the tags for
-        # this converter are named the same other than the version number.
-        tags = list(sorted(tags, reverse=True))
-        return tags[0]
+    types = ["ndcube.ndcube.NDCube"]
 
     def from_yaml_tree(self, node, tag, ctx):
         from ndcube import ndcube
@@ -25,6 +14,6 @@ class NDCubeConverter(Converter):
 
     def to_yaml_tree(self, ndcube, tag, ctx):
         node = {}
-        node["data"] = np.asarray(ndcube.data)
+        node["data"] =np.array(ndcube.data)
         node["wcs"] = ndcube.wcs
         return node
