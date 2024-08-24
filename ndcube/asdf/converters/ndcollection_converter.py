24af793cc51c0cff8569710b8887655ea699fe12
ViciousEagle03 <piyushsharma04321@gmail.com>
2024-08-25 05:13:30 +0530

    Add serialization logic for the NDCubeSequence and NDCollection

---
+++ ndcube/asdf/converters/ndcollection_converter.py
@@ -0,0 +1,30 @@
+from asdf.extension import Converter
+
+
+class NDCollectionConverter(Converter):
+    tags = ["tag:sunpy.org:ndcube/ndcube/ndcollection-*"]
+    types = ["ndcube.ndcollection.NDCollection"]
+
+    def from_yaml_tree(self, node, tag, ctx):
+        from ndcube.ndcollection import NDCollection
+
+        key_value_pairs = list(zip(node["keys"], node["value"]))
+        aligned_axes = list(node.get("aligned_axes").values())
+        aligned_axes = tuple(tuple(lst) for lst in aligned_axes)
+        ndcollection = NDCollection(key_value_pairs,
+                            meta=node.get("meta"),
+                            aligned_axes = aligned_axes)
+        return ndcollection
+
+
+    def to_yaml_tree(self, ndcollection, tag, ctx):
+
+        node = {}
+        node["keys"] = tuple(ndcollection.keys())
+        node["value"] = tuple(ndcollection.values())
+        if ndcollection.meta is not None:
+            node["meta"] = ndcollection.meta
+        if ndcollection._aligned_axes is not None:
+            node["aligned_axes"] = ndcollection._aligned_axes
+
+        return node
---
+++ ndcube/asdf/converters/ndcubesequence_converter.py
@@ -0,0 +1,25 @@
+from asdf.extension import Converter
+
+
+class NDCubeSequenceConverter(Converter):
+    tags = ["tag:sunpy.org:ndcube/ndcube/ndcube_sequence-*"]
+    types = ["ndcube.ndcube_sequence.NDCubeSequence"]
+
+    def from_yaml_tree(self, node, tag, ctx):
+        from ndcube.ndcube_sequence import NDCubeSequence
+
+        return NDCubeSequence(node["data"],
+                              meta=node.get("meta"),
+                              common_axis=node.get("common_axis"))
+
+
+    def to_yaml_tree(self, ndcseq, tag, ctx):
+
+        node = {}
+        node["data"] = ndcseq.data
+        if ndcseq.meta is not None:
+            node["meta"] = ndcseq.meta
+        if ndcseq._common_axis is not None:
+            node["common_axis"] = ndcseq._common_axis
+
+        return node
