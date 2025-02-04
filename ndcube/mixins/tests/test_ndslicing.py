0af338cae7189d69595a57064067a1d7918d1cc9
Diya <khetarpaldiya@gmail.com>
2025-02-04 11:14:33 +0530

    Updated test file by using pre defined fixture

--- ndcube/mixins/tests/test_ndslicing.py
+++ ndcube/mixins/tests/test_ndslicing.py
@@ -1,20 +1,22 @@
+<<<<<<< HEAD
 import numpy as np
 import pytest
 
 from astropy.wcs import WCS
+=======
+import pytest
+>>>>>>> 9f68a49 (Updated test file by using pre defined fixture)
 
 from ndcube import NDCube
 
 
 @pytest.fixture
-def sample_ndcube():
-    data = np.random.rand(4, 4, 4)
-    wcs = WCS(naxis=3)
-    return NDCube(data, wcs)
+def sample_ndcube(ndcube_4d_ln_l_t_lt):
+    return ndcube_4d_ln_l_t_lt
 
 def test_ellipsis_usage(sample_ndcube):
     sliced_cube = sample_ndcube[..., 1]
-    assert sliced_cube.data.shape == (4, 4)
+    assert sliced_cube.data.shape == (5,10,12)
 
     with pytest.raises(IndexError, match="An index can only have a single ellipsis"):
         sample_ndcube[..., ..., 1]
