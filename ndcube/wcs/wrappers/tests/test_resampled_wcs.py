47e438f648dff03decf5c5f1e96caf79d71e9ecc
DanRyanIrish <ryand5@tcd.ie>
2025-08-12 18:21:36 +0100

    Fix test_2d() since ResampledLowLevelWCS bug has been fixed.

--- ndcube/wcs/wrappers/tests/test_resampled_wcs.py
+++ ndcube/wcs/wrappers/tests/test_resampled_wcs.py
@@ -26,8 +26,8 @@ This transformation has 2 pixel and 2 world dimensions
 Array shape (Numpy order): (2, 15)
 
 Pixel Dim  Axis Name  Data size  Bounds
-        0  None              15  (-2.5, 12.5)
-        1  None         2.33333  (0.3333333333333333, 2.3333333333333335)
+        0  None              15  (-1.75, 13.25)
+        1  None         2.33333  (0.0, 2.0)
 
 World Dim  Axis Name        Physical Type  Units
         0  Right Ascension  pos.eq.ra      deg
@@ -48,8 +48,8 @@ This transformation has 2 pixel and 2 world dimensions
 Array shape (Numpy order): (2.3333333333333335, 15.0)
 
 Pixel Dim  Axis Name  Data size  Bounds
-        0  None              15  (-2.5, 12.5)
-        1  None         2.33333  (0.3333333333333333, 2.3333333333333335)
+        0  None              15  (-1.75, 13.25)
+        1  None         2.33333  (0.0, 2.0)
 
 World Dim  Axis Name        Physical Type  Units
         0  Right Ascension  pos.eq.ra      deg
@@ -70,7 +70,8 @@ def test_2d(celestial_wcs):
 
     # Upsample along the first pixel dimension and downsample along the second
     # pixel dimension.
-    wcs = ResampledLowLevelWCS(celestial_wcs, [0.4, 3])
+    factors = [0.4, 3]
+    wcs = ResampledLowLevelWCS(celestial_wcs, factors)
 
     # The following shouldn't change compared to the original WCS
     assert wcs.pixel_n_dim == 2
@@ -78,45 +79,48 @@ def test_2d(celestial_wcs):
     assert tuple(wcs.world_axis_physical_types) == ('pos.eq.ra', 'pos.eq.dec')
     assert tuple(wcs.world_axis_units) == ('deg', 'deg')
     assert tuple(wcs.pixel_axis_names) == ('', '')
-    assert tuple(wcs.world_axis_names) == ('Right Ascension',
-                                           'Declination')
+    assert tuple(wcs.world_axis_names) == ('Right Ascension', 'Declination')
     assert_equal(wcs.axis_correlation_matrix, np.ones((2, 2)))
 
     # Shapes and bounds should be floating-point if needed
     assert_allclose(wcs.pixel_shape, (15, 7/3))
     assert_allclose(wcs.array_shape, (7/3, 15))
-    assert_allclose(wcs.pixel_bounds, ((-2.5, 12.5), (1/3, 7/3)))
+    assert_allclose(wcs.pixel_bounds, ((-1.75, 13.25), (0, 2)))
 
-    pixel_scalar = (2.3, 1.2)
-    world_scalar = (12.16, -4.8)
-    assert_allclose(wcs.pixel_to_world_values(*pixel_scalar), world_scalar)
-    assert_allclose(wcs.array_index_to_world_values(*pixel_scalar[::-1]), world_scalar)
-    assert_allclose(wcs.world_to_pixel_values(*world_scalar), pixel_scalar)
-    assert_allclose(wcs.world_to_array_index_values(*world_scalar), [1, 2])
+    under_pixel, over_pixel = (2.3, 1.2), (5.75+0.6*1.25, 0.2/1.5*0.5)
+    world = celestial_wcs.pixel_to_world_values(*under_pixel)
+
+    assert_allclose(wcs.pixel_to_world_values(*over_pixel), world)
+    assert_allclose(wcs.array_index_to_world_values(*over_pixel[::-1]), world)
+    assert_allclose(wcs.world_to_pixel_values(*world), over_pixel)
+    assert_allclose(wcs.world_to_array_index_values(*world),
+                    np.around(over_pixel[::-1]).astype(int))
 
     EXPECTED_2D_REPR = EXPECTED_2D_REPR_NUMPY2 if np.__version__ >= '2.0.0' else EXPECTED_2D_REPR_NUMPY1
     assert str(wcs) == EXPECTED_2D_REPR
     assert EXPECTED_2D_REPR in repr(wcs)
 
     celestial_wcs.pixel_bounds = None
-    pixel_array = (np.array([2.3, 2.4]),
-                   np.array([4.3, 4.4]))
-    world_array = (np.array([12.16, 12.08]),
-                   np.array([13.8, 14.4]))
-    assert_allclose(wcs.pixel_to_world_values(*pixel_array), world_array)
-    assert_allclose(wcs.array_index_to_world_values(*pixel_array[::-1]), world_array)
-    assert_allclose(wcs.world_to_pixel_values(*world_array), pixel_array)
+    under_pixel_array = (np.array([2.3, 2.4]),
+                         np.array([4.3, 4.4]))
+    over_pixel_array = (np.array([over_pixel[0], 5.75+0.8*1.25]),
+                        1 + np.array([0.3, 0.4]) / 1.5 * 0.5)
+    world_array = celestial_wcs.pixel_to_world_values(*under_pixel_array)
+
+    assert_allclose(wcs.pixel_to_world_values(*over_pixel_array), world_array)
+    assert_allclose(wcs.array_index_to_world_values(*over_pixel_array[::-1]), world_array)
+    assert_allclose(wcs.world_to_pixel_values(*world_array), over_pixel_array)
     assert_allclose(wcs.world_to_array_index_values(*world_array),
-                    [[4, 4], [2, 2]])
+                    np.around(np.asarray(over_pixel_array)[::-1]).astype(int))
 
     wcs_hl = HighLevelWCSWrapper(wcs)
 
-    celestial = wcs_hl.pixel_to_world(*pixel_scalar)
+    celestial = wcs_hl.pixel_to_world(*over_pixel)
     assert isinstance(celestial, SkyCoord)
-    assert_quantity_allclose(celestial.ra, world_scalar[0] * u.deg)
-    assert_quantity_allclose(celestial.dec, world_scalar[1] * u.deg)
+    assert_quantity_allclose(celestial.ra, world[0] * u.deg)
+    assert_quantity_allclose(celestial.dec, world[1] * u.deg)
 
-    celestial = wcs_hl.pixel_to_world(*pixel_array)
+    celestial = wcs_hl.pixel_to_world(*over_pixel_array)
     assert isinstance(celestial, SkyCoord)
     assert_quantity_allclose(celestial.ra, world_array[0] * u.deg)
     assert_quantity_allclose(celestial.dec, world_array[1] * u.deg)
