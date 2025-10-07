cc8d915b35f53d4c27521f06b94167f01c310925
Stuart Mumford <stuart@cadair.com>
2025-10-07 13:51:02 +0100

    Fix some tests requiring dev asdf-astropy

--- ndcube/asdf/converters/tests/test_ndcube_converter.py
+++ ndcube/asdf/converters/tests/test_ndcube_converter.py
@@ -11,7 +11,7 @@ from ndcube.tests.helpers import assert_cubes_equal
 
 def test_serialization(all_ndcubes, tmp_path, all_ndcubes_names):
     # asdf_astropy doesn't save _naxis before this PR: https://github.com/astropy/asdf-astropy/pull/276
-    if Version(asdf_astropy.__version__) < Version("0.8.0") and isinstance(all_ndcubes.wcs, astropy.wcs.WCS):
+    if Version(asdf_astropy.__version__) < Version("0.9.0") and isinstance(all_ndcubes.wcs, astropy.wcs.WCS):
         all_ndcubes.wcs._naxis = [0, 0]
 
     if not isinstance(all_ndcubes.data, np.ndarray):
@@ -28,8 +28,8 @@ def test_serialization(all_ndcubes, tmp_path, all_ndcubes_names):
 
 @pytest.mark.parametrize("expected_cube", ["ndcube_gwcs_3d_ln_lt_l", "ndcube_3d_ln_lt_l"], indirect=True)
 def test_serialization_sliced_ndcube(expected_cube, tmp_path):
-    # This needs 0.8.0 of asdf_astropy to be able to save gwcs and to save array_shape on WCS
-    pytest.importorskip("asdf_astropy", "0.8.0")
+    # This needs 0.9.0 of asdf_astropy to be able to save gwcs and to save array_shape on WCS
+    pytest.importorskip("asdf_astropy", "0.9.0")
 
     sndc = expected_cube[np.s_[0, :, :]]
     file_path = tmp_path / "test.asdf"
